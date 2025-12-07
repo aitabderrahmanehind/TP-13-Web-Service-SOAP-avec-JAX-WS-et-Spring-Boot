2006f2015d7d766178636a1fea01dfc704c02523
aitabderrahmanehind <aitabderrahmanehind@gmail.com>
2025-12-07 15:17:53 +0100

    Tp 13

---
+++ src/main/java/org/example/demo/config/entities/CompteRepository.java
@@ -0,0 +1,9 @@
+package org.example.demo.config.entities;
+
+import org.example.demo.config.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
---
+++ src/main/java/org/example/demo/config/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+package org.example.demo.config.entities;
+
+public enum TypeCompte {
+    DEBIT , CREDIT ;
+}
